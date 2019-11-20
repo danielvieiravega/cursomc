3aae333998a5d29eb8ef2021692727d3667c8b84
Daniel Vieira Vega <vega@umbler.com>
2019-11-20 07:58:20 -0300

    Projeto criado

---
+++ src/test/java/com/danielvieiravega/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielvieiravega.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
